3ad09e74b7993479a68b3ea24d16cfe7d6e9f5b7
danielay <danielitayero@gmail.com>
2023-08-26 17:12:13 -0400

    feat: add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 
 import './App.css'
-import AboutMe from './components/aboutMe/AboutMe'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
-import NavBar from './components/navBar/NavBar'
 import Projects from './components/projects/Projects'
-import StarsSkay from './components/starsSky/StarsSkay'
-import About from './components/about/About'
-
-
-
+import {AnimatePresence} from 'framer-motion'
 
 
 function App() {
   return (
-    <>
+    < AnimatePresence>
     <Router>
         <Routes>
               <Route path = "/" element = { <Home/> } />
               <Route path = "/about" element = {<Projects/>}/>
         </Routes>
   </Router>
-
-  
-
-     
-     
-    </>
+ 
+    </AnimatePresence>
   )
 }
 
