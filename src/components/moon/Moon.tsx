72a10e8c0b48852c0c785ca8cf837d54b1a0bb49
danielay <danielitayero@gmail.com>
2023-09-20 22:07:26 -0400

    feat: make reusable moon component

--- src/components/moon/Moon.tsx
+++ src/components/moon/Moon.tsx
@@ -1,12 +1,12 @@
 import "./Moon.css"
 
-function Moon( props:{ moonSize: number, light1: number, light2: number }) {
+function Moon( props:{ moonSize: number; light1: number; light2: number; iconsize: number }) {
     return (
         <div className="moonBx2">
-        <div className="moon4" style ={{ "--moonSize": {moonSize}} as React.CSSProperties}> </div>
-        <div className="moon5" ></div>
-        <div className="moon6"> </div>
-        <img src="moon.svg" alt="" />
+        <div className="moon4" style ={{ "--moonSize": `${props.moonSize}px`} as React.CSSProperties}></div>
+        <div className="moon5" style ={{ "--light1": `${props.light1}px`} as React.CSSProperties}></div>
+        <div className="moon6" style ={{ "--light2": `${props.light2}px`} as React.CSSProperties}> </div>
+        <img src="/moon.svg" alt="" style ={{ "--iconsize": `${props.iconsize}px`} as React.CSSProperties}/>
         
     </div>  
     );
