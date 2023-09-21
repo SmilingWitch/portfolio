aada9fb920ca2a3bca15b91b1e7a9e6a1781b132
danielay <danielitayero@gmail.com>
2023-09-20 22:05:07 -0400

    feat: improve contact component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,4 @@
+import Moon from "../moon/Moon";
 import TextInCircle from "../texInCircle/TextInCircle";
 import  "./Contact.css";
 
@@ -7,16 +8,25 @@ function Contact() {
                 <div className="header1 h">
                     Contact
                 </div>
+                    <div className="contactHeader">
+                        Let’s Build Something Together
+                    </div>
+                    <div className="socialMedia1">
+                        <div className="email">
+                            danielitayero@gmail.com
+                        </div>
+                    </div>
+            
                 <div className="moonBxSP">
-                    <div className="moon1SP"  ></div>
-                    <div className="moon2SP"  ></div>
-                    <div className="moon3SP"> </div>
-                    <img src='moon.svg' alt="" />
+                <Moon moonSize= {180} light1= {140} light2 = {100} iconsize = {145}/>
                     <div className="socialMedia">
                         <TextInCircle></TextInCircle> 
                     </div>
                 </div>
             </div>
+
+
+
     );
 }
 
